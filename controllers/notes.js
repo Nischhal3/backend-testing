d846b7d3d86460c8cc823bf11bd524761aaa6c31
Nischhal <nischhal3@gmail.com>
2021-06-23 20:12:44 +0300

    note-backend-testing

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,49 +1,39 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes.map(note => note.toJSON()))
-  })
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({});
+  response.json(notes.map(note => note.toJSON()));
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note.toJSON())
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
-})
-
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined ? false : body.important,
     date: new Date(),
   })
+  const savedNote = await note.save()
+  response.json(savedNote.toJSO())
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote.toJSON())
-    })
-    .catch(error => next(error))
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+notesRouter.get('/:id', async (request, response, next) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note.toJSON())
+  } else {
+    response.status(404).end()
+  }
 })
 
-notesRouter.put('/:id', (request, response, next) => {
+notesRouter.delete('/:id', async (request, response, next) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+notesRouter.put('/:id', async (request, response, next) => {
   const body = request.body
 
   const note = {
@@ -51,11 +41,8 @@ notesRouter.put('/:id', (request, response, next) => {
     important: body.important,
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote.toJSON())
-    })
-    .catch(error => next(error))
+  const noteToupdate = await Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  response.json(noteToupdate.toJSON())
 })
 
 module.exports = notesRouter
---
+++ tests/palindrome.test.js
@@ -0,0 +1,36 @@
+const palindrome = require('../utils/for_testing').palindrome
+const average = require('../utils/for_testing').average
+
+describe('palindrome', () => {
+    test('palindrome of a ', () => {
+        const result = palindrome('a');
+
+        expect(result).toBe('a');
+    })
+
+    test('palindrome of react', () => {
+        const result = palindrome('react');
+
+        expect(result).toBe('tcaer');
+    })
+
+    test('palindrome of saippuakauppias', () => {
+        const result = palindrome('saippuakauppias');
+
+        expect(result).toBe('saippuakauppias')
+    })
+})
+
+describe('average', () => {
+    test('of one value is the value itself', () => {
+        expect(average([1])).toBe(1)
+    })
+
+    test('of many is calculated right', () => {
+        expect(average([1, 2, 3, 4, 5, 6])).toBe(3.5)
+    })
+
+    test('of empty array is zero', () => {
+        expect(average([])).toBe(0)
+    })
+})
